376ddee468590108dadf1dcc55a1a883a813fdc5
Prashant-Shekhar-Rao <139104389+Prashant-Shekhar-Rao@users.noreply.github.com>
2024-02-25 23:59:22 +0530

    Delete target/debug directory

--- target/debug/boltzman.d
+++
@@ -1,1 +0,0 @@
-D:\projects\boltzman\target\debug\boltzman.exe: D:\projects\boltzman\src\main.rs
--- target/debug/deps/boltzman.d
+++
@@ -1,5 +0,0 @@
-D:\projects\boltzman\target\debug\deps\boltzman.exe: src\main.rs
-
-D:\projects\boltzman\target\debug\deps\boltzman.d: src\main.rs
-
-src\main.rs:
